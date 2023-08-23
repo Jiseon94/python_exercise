6a4a36d8667067388d4301c6459756c6dff7d1ba
Jiseon94 <hjsun2025@gmail.com>
2023-08-24 04:15:45 +0900

    230824 - for

---
+++ 131 ~ 200 (while, for)/ex171_180.py
@@ -0,0 +1,17 @@
+#171
+price_list = [32100, 32150, 32000, 32500]
+for i in range(0,4):
+    print(price_list[i])
+
+for i in range(len(price_list)):
+    print(price_list[i])
+
+#172
+for i in range(len(price_list)):
+    print(i,price_list[i])
+
+#173
+for i in range(len(price_list)):
+    print(len(price_list)-i,price_list[i])
+
+#174
