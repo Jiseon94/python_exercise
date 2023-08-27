ee4136f8815dcc6f13058a566c55700533a4c456
Jiseon94 <hjsun2025@gmail.com>
2023-08-27 23:33:48 +0900

    230827 - for

--- 131 ~ 200 (while, for)/ex171_180.py
+++ 131 ~ 200 (while, for)/ex171_180.py
@@ -17,3 +17,12 @@ for i in range(len(price_list)):
 #174
 for i in range(1,4):
     print(90 + 10*i, price_list[i])
+
+#175
+my_list = ["가", "나", "다", "라"]
+for i in range(0,3):
+    print(my_list[i], my_list[i+1])
+
+#176
+my_list = ["가", "나", "다", "라", "마"]
+
