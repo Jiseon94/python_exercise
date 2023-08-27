548da0d0b3afc47dafce9d01039f3c00c009a91f
Jiseon94 <hjsun2025@gmail.com>
2023-08-27 23:59:27 +0900

    230827 - for

---
+++ 131 ~ 200 (while, for)/ex181_190.py
@@ -0,0 +1,12 @@
+#181
+apart =[]
+# apart.append(("101호", "102호"))
+# apart.append(("201호", "202호"))
+# apart.append(("301호", "302호"))
+apart.append(["101호", "102호"])
+apart.append(["201호", "202호"])
+apart.append(["301호", "302호"])
+print(apart)
+
+
+#182
