7ecdd102b85b49b4226222bedc0ebefff5948322
Jiseon94 <hjsun2025@gmail.com>
2023-08-29 16:24:03 +0900

    230829 - 함수개념(Lambda)

--- jump_to_python/def_03.py
+++ jump_to_python/def_03.py
@@ -1,1 +1,12 @@
 #Lambda
+# 함수를 간단하게 표현하는 방법.
+# def add(a,b):
+#     return a+b
+
+
+add = lambda a, b: a+b
+print(add(1,2))
+
+myList = [lambda a, b: a+b, lambda a, b:a*b]
+
+print(myList[1](1,2))   #2
