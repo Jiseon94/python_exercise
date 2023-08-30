734d8a4cddcb3ea8f39a7ac78dc1f9a11b2a7f25
Jiseon94 <hjsun2025@gmail.com>
2023-08-30 23:39:15 +0900

    230830 - 함수

--- 201 ~ 240 (def)/ex201_210.py
+++ 201 ~ 240 (def)/ex201_210.py
@@ -11,3 +11,24 @@ for i in range(100):
     print_coin()
 
 #204
+def print_coins():
+    for i in range(100):
+        print("비트코인")
+
+#205
+#함수가 정의 되기 전에 함수를 실행한 셈이라서
+
+#206
+#A B C A B
+
+#207
+#A C B
+
+#208
+#A C B E D
+
+#209
+#B A
+
+#210
+#BCBCBCA
